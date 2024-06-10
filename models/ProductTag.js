46ac317e2e54c4af6d128bd001244aaac4eda7bf
Joseph Ruff <joegruff16@gmail.com>
2024-06-09 22:37:09 -0400

    Completed defining all columns and model attributes for all models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,19 +1,40 @@
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
+const { INTEGER } = require("sequelize");
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // define columns and model attributes
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        id: "id",
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        id: "id",
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product_tag',
+    modelName: "product_tag",
   }
 );
 
