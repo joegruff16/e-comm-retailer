9c1ae5b3c0043a2500c7b0ea67df9e12aa48de51
Joseph Ruff <joegruff16@gmail.com>
2024-06-23 18:10:24 -0400

    Added a delete route that will delete a category by it's id.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -62,6 +62,13 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: { id: req.params.id },
+  })
+    .then((deletedCategory) => res.status(200).json(deletedCategory))
+    .catch((err) => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
