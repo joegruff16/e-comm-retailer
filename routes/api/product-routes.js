d3a28fc61421d1ea902ddd2786eaa6cb4f5a5e45
Joseph Ruff <joegruff16@gmail.com>
2024-06-23 18:24:54 -0400

    Added a delete route that will delete a product by a specified id.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -112,6 +112,13 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: { id: req.params.id },
+  })
+    .then((deletedProduct) => res.status(200).json(deletedProduct))
+    .catch((err) => {
+      res.json(500).json(err);
+    });
 });
 
 module.exports = router;
